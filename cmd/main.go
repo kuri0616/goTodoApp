8ed3e64b81f935e91eaebe6446e183502c0175d7
日下陸矢 <rikuya.98@gmail.com>
2024-05-05 19:44:44 +0900

    　gorillaのinstall、仮のハンドラーを作成

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+func getTodoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("get Todo"))
+}
+func postTodoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("post Todo"))
+}
+func putTodoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("put Todo"))
+}
+func deleteTodoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("delete Todo"))
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/todo", getTodoHandler).Methods(http.MethodGet)
+	r.HandleFunc("/todo", postTodoHandler).Methods(http.MethodPost)
+	r.HandleFunc("/todo", putTodoHandler).Methods(http.MethodPut)
+	r.HandleFunc("/todo", deleteTodoHandler).Methods(http.MethodDelete)
+
+	log.Println("Server started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
