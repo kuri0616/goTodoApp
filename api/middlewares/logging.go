2ff093f5d3a9e629ec7f9b25fd3e9cd435fbb985
日下陸矢 <rikuya.98@gmail.com>
2024-05-18 14:53:53 +0900

    　レスポンスの内容をログに出力する機能を追加　

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,9 +5,25 @@ import (
 	"net/http"
 )
 
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func newResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{w, http.StatusOK}
+}
+
+func (w *resLoggingWriter) WriteHeader(code int) {
+	w.code = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.Method, r.RequestURI)
-		next.ServeHTTP(w, r)
+		resWriter := newResLoggingWriter(w)
+		next.ServeHTTP(resWriter, r)
+		log.Println("res :", resWriter.code)
 	})
 }
